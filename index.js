2d8da43aa5fda860c180db2cdb524fdbeba4cfed
CharPet <137006290+CharPet@users.noreply.github.com>
2025-11-09 13:32:20 +0200

    Added if...else statement

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 let firstCard = 4;
 let secondCard = 11;
 let sum = firstCard + secondCard;
+
+// console.log(sum);
+
+// ------------------------------
+
+if (sum < 21) {
+  console.log("Do you want to draw a new card?");
+} else if (sum === 21) {
+  console.log("Wohoo! You've got Blackjack!");
+} else if (sum > 21) {
+  console.log("You're out of the game!");
+}
