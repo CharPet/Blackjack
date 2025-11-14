fadbbb567065da1316e3e8437177b89d07fd897f
CharPet <137006290+CharPet@users.noreply.github.com>
2025-11-14 20:27:08 +0200

    Finished array exercises

---
+++ index2.js
@@ -0,0 +1,40 @@
+let featuredPosts = [
+  "Check out my Netflix clone",
+  "Here's the code for my project",
+  "I've just relaunched my portfolio",
+];
+
+// console.log(featuredPosts[2]);
+console.log(featuredPosts.length);
+
+// ------------------------------
+
+// let lifeExperience = ["One", "Two", "Three", "Four", "Five"];
+
+// console.log(lifeExperience[3]);
+// console.log(lifeExperience[2]);
+// console.log(lifeExperience[0]);
+
+// ------------------------------
+
+let yourself = ["Pepe", 68, true];
+
+// ------------------------------
+
+let messages = [
+  "Hey, how's it going?",
+  "I'm great, thank you! How about you?",
+  "All good. Been working on some code lately.",
+];
+
+let newMessage = "Same here!";
+
+messages.push(newMessage);
+
+console.log(messages);
+
+// ------------------------------
+
+messages.pop();
+
+console.log(messages);
