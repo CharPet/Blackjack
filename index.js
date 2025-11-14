e325e96b1060fa6e3f2b743082fb0ffbb5d721f4
CharPet <137006290+CharPet@users.noreply.github.com>
2025-11-14 19:43:43 +0200

    Added a 'New Card' button and it's function

--- index.js
+++ index.js
@@ -71,11 +71,12 @@ console.log(3 <= 2); //false
 // ------------------------------
 
 let firstCard = 10;
-let secondCard = 7;
+let secondCard = 4;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let card = 7;
 
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
@@ -83,8 +84,8 @@ let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
-  sumEl.textContent = "Sum: " + sum;
   cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+  sumEl.textContent = "Sum: " + sum;
 
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
@@ -102,4 +103,12 @@ function startGame() {
   // console.log(hasBlackJack);
   // console.log(isAlive);
   messageEl.textContent = message;
+}
+
+function newCard() {
+  console.log("Drawing a new card from the deck!");
+
+  sum += card;
+
+  startGame();
 }
